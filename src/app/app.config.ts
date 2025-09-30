16a111238f8265bd30180921f916b921d41bc38c
AlexAraldi <alexanderaraldi@gmail.com>
2025-09-30 16:51:11 -0300

    Implementada versão inicial do jogo

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+
+//configura a injeção de dependêcia da apilicação
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true })
+  ]
+};
---
+++ src/app/app.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.html',
+ 
+})
+export class App implements OnInit {
+  public numeroDigitado: number = 1;
+  public numeroSecreto: number = 0;
+
+  public jogoEstaFinalizado: boolean = false;
+
+  public dicaNumeroMaiorQue: number = 1;
+  public dicaNumeroMenorQue: number = 100;
+
+  ngOnInit(): void {
+    this.numeroSecreto = this.obterNumeroSecreto(1,100);
+  }
+  public adivinhar(): void{
+    if (this.numeroDigitado < this.numeroSecreto)
+      this.dicaNumeroMaiorQue = this.numeroDigitado;
+      else if (this.numeroDigitado > this.numeroSecreto)
+        this.dicaNumeroMenorQue = this.numeroDigitado;
+
+      else
+        this.jogoEstaFinalizado = true;
+
+  }
+  public reiniciar():void {
+    this.numeroDigitado = 1;
+    this.dicaNumeroMaiorQue = 1;
+    this.dicaNumeroMenorQue = 100;
+    this.jogoEstaFinalizado = false;
+    this.numeroSecreto = this.obterNumeroSecreto(1,100);
+  }
+  private obterNumeroSecreto(min: number , max: number){
+    const numeroAleatorio: number = Math.random() * 100;
+
+    const numeroSecreto = Math.floor(numeroAleatorio);
+
+    console.log(numeroSecreto);
+    
+    return numeroSecreto;
+  }
+}
