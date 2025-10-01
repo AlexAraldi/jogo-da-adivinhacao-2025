ba33c4c29b515734dfbd3aadcd41d3a2e30b7967
AlexAraldi <alexanderaraldi@gmail.com>
2025-10-01 11:17:19 -0300

    implementada feature selecionarDificuldade

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,10 +15,42 @@ export class App implements OnInit {
   public dicaNumeroMaiorQue: number = 1;
   public dicaNumeroMenorQue: number = 100;
 
-  ngOnInit(): void {
-    this.numeroSecreto = this.obterNumeroSecreto();
+  public dificuldadeSelecionada?: string;
+  public tentativasRestantes: number = 0;
+
+  ngOnInit(): void {}
+
+  public selecionarDificuldade(dificuldade: string): void {
+    switch (dificuldade) {
+      case 'Fácil':
+        this.numeroSecreto = this.obterNumeroSecreto(10);
+        this.dicaNumeroMenorQue = 10;
+        this.tentativasRestantes = 3;
+        break;
+
+      case 'Médio':
+        this.numeroSecreto = this.obterNumeroSecreto(50);
+        this.dicaNumeroMenorQue = 50;
+        this.tentativasRestantes = 6;
+        break;
+
+      case 'Difícil':
+        this.numeroSecreto = this.obterNumeroSecreto(100);
+        this.dicaNumeroMenorQue = 100;
+        this.tentativasRestantes = 7;
+        break;
+    }
+
+    this.dificuldadeSelecionada = dificuldade;
   }
+
   public adivinhar(): void {
+    this.tentativasRestantes--;
+    if (this.tentativasRestantes <= 0) {
+      this.jogoEstaFinalizado = true;
+      return;
+    }
+
     if (this.numeroDigitado < this.numeroSecreto) this.dicaNumeroMaiorQue = this.numeroDigitado;
     else if (this.numeroDigitado > this.numeroSecreto)
       this.dicaNumeroMenorQue = this.numeroDigitado;
@@ -29,10 +61,10 @@ export class App implements OnInit {
     this.dicaNumeroMaiorQue = 1;
     this.dicaNumeroMenorQue = 100;
     this.jogoEstaFinalizado = false;
-    this.numeroSecreto = this.obterNumeroSecreto();
+    this.dificuldadeSelecionada = undefined;
   }
-  private obterNumeroSecreto() {
-    const numeroAleatorio: number = Math.random() * 100;
+  private obterNumeroSecreto(max: number) {
+    const numeroAleatorio: number = Math.random() * (max - 1) + 1;
 
     const numeroSecreto = Math.floor(numeroAleatorio);
 
